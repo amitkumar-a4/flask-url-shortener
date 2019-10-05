b6d78ae221bcc8bdf94db76f856919dfe13bd021
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-10-05 12:47:28 -0500

    Add test for app config

---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+def test_testing_config(app):
+    assert app
+    assert app.config["DEBUG"]
+    assert app.config["TESTING"]
+    assert not app.config["PRESERVE_CONTEXT_ON_EXCEPTION"]
+    assert app.config["SQLALCHEMY_DATABASE_URI"]
