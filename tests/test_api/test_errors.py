a40b828dcaa990255c95c70c297313e1b2c626aa
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-10-05 13:00:12 -0500

    Add missing-endpoint error test

---
+++ tests/test_api/test_errors.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+
+
+def test_missing_url(client):
+    """ Missing endpoint error test"""
+    response = client.get('/missing-url')
+    print('estes', response.json)
+    assert response.status_code == HTTPStatus.NOT_FOUND, 'Improper response'
+    assert response.json['error']
+    assert response.json['code']
+    assert response.json['message']
