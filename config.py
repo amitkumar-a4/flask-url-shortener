aebd0e0dc6a8afff504a30aca8fdf1723ddc2220
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-10-05 12:44:56 -0500

    Add DB cleanup after testing

--- config.py
+++ config.py
@@ -2,17 +2,20 @@ import os
 from dotenv import load_dotenv
 
 # Load configuration from .env file
-stage = os.getenv('FLASK_ENV') or 'dev'
+stage = os.getenv('FLASK_ENV') or 'development'
 if stage == 'PROD':
-  dotenv_file = '.env.prod'
+    dotenv_file = '.env.prod'
+elif stage == 'TEST':
+    dotenv_file = '.env.test'
 else:
-  dotenv_file = '.env.dev'
+    dotenv_file = '.env.dev'
 
 load_dotenv(dotenv_file)
+
 
 class Base:
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     DEBUG = os.getenv('DEBUG')
     TESTING = os.getenv('TESTING') or False
-    SQLALCHEMY_DATABASE_URI= os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(PROJECT_ROOT, 'dev.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_ROOT, os.getenv('SQLALCHEMY_DATABASE_URI'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- conftest.py
+++ conftest.py
@@ -1,8 +1,20 @@
 import pytest
+import os
+
 from flask import Flask
+
 
 @pytest.fixture
 def app() -> Flask:
-    from app import create_app
     """ Provides an instance of our Flask app """
+    from app import create_app
     return create_app()
+
+@pytest.fixture(scope="package", autouse=True)
+def cleanup():
+    """Cleanup a db once we are finished."""
+    try:
+        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+        os.remove(os.path.join(PROJECT_ROOT, 'test.db'))
+    except FileNotFoundError:
+        pass
