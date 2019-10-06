661782b8aa6fed7b15dc9f855ec91f2ae23b2c8a
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-10-06 18:13:14 -0500

    Create GET redirect_to_long_url endpoint

--- app/short_urls/routes.py
+++ app/short_urls/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, abort, request, jsonify, make_response
+from flask import Blueprint, abort, request, jsonify, make_response, redirect
 from http import HTTPStatus
 
 from app.users.model import User
 from app.short_urls.model import Url
 from app.short_urls.constants import SHORT_URL_CREATED
+
 # Declare the blueprint
 short_urls_bp = Blueprint('short_urls', __name__)
 
@@ -23,12 +24,23 @@ def register_url():
     new_short_url = Url(
       user=current_user,
       long_url=url,
-      short_url=Url._rand_id())
-    short_url = new_short_url.save()
+      short_id=Url.create_id())
+    new_short_url.save()
 
     response_body = {
             "message": SHORT_URL_CREATED,
-            "data": short_url.to_dict()
+            "data": {
+              'short_url': new_short_url.get_short_url()
+            }
         }
     response = make_response(jsonify(response_body), HTTPStatus.CREATED)
     return response
+
+
+@short_urls_bp.route('/<short_url>', methods=['GET'])
+def redirect_to_long_url(short_url):
+
+    url = Url.get_by_short_url(short_url)
+    if not url:
+        abort(HTTPStatus.NOT_FOUND, 'url does not exist')
+    return redirect(url.long_url, code=HTTPStatus.PERMANENT_REDIRECT)
