dbef22b75b7b1aad79a50c4d73020cd780f07852
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-10-08 19:43:16 -0500

    Refactor config file

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 
 # Load configuration from .env file
-stage = os.getenv('FLASK_ENV') or 'development'
-if stage == 'PROD':
-    dotenv_file = '.env.prod'
-elif stage == 'TEST':
+stage = os.getenv('FLASK_ENV')
+if stage == 'development':
+    dotenv_file = '.env.dev'
+elif stage == 'test':
     dotenv_file = '.env.test'
 else:
-    dotenv_file = '.env.dev'
+    dotenv_file = '.env.prod'
 
 load_dotenv(dotenv_file)
 
@@ -17,6 +17,10 @@ class Base:
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     DEBUG = os.getenv('DEBUG')
     TESTING = os.getenv('TESTING') or False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_ROOT, os.getenv('SQLALCHEMY_DATABASE_URI'))
+    if stage == 'development' or stage == 'test':
+        SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(PROJECT_ROOT,
+                                   os.getenv('SQLALCHEMY_DATABASE_URI')))
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
